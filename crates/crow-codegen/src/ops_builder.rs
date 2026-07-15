3db3888116aec853fa989b3c73f6a5510f880c75
kie <eznaevsamil70@gmail.com>
2026-07-15 08:22:21 +0300

    fix(codegen): use ordered float predicates and bug macro for invalid states
    Replace unordered float predicates UEQ and UNE with ordered OEQ and ONE
    in eq and not-eq comparisons for consistency with other comparison builders
    
    Introduce generic unwrap_or_bug helper wrapping builder Result unwrapping
    and reporting failures through the bug macro closing the ops wrappers todo
    
    Replace raw panic macro calls in unreachable type branches with bug macro
    making the previously unused crow_common bug import actually used
    
    Fix copied error message text in build_le function referring wrongly
    to not equal operation instead of less equal operation

--- crates/crow-codegen/src/ops_builder.rs
+++ crates/crow-codegen/src/ops_builder.rs
@@ -3,108 +3,110 @@ use crow_mir::BinOp;
 use crow_types::Ty;
 use inkwell::{FloatPredicate, IntPredicate, builder::Builder, values::{BasicValue, BasicValueEnum}};
 
-//todo - ops wrappers for panic
+fn unwrap_or_bug<'llvm, T, E: std::fmt::Debug>(result: Result<T, E>, op: &str) -> T {
+    result.unwrap_or_else(|e| bug!("failed to build {} instruction: {:?}", op, e))
+}
 
 fn build_sum<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => 
-            builder.build_int_add(lhs.into_int_value(), rhs.into_int_value(), "int_add")
-            .unwrap().as_basic_value_enum(),
-        Ty::Float(_) => 
-            builder.build_float_add(lhs.into_float_value(), rhs.into_float_value(), "float_add")
-            .unwrap().as_basic_value_enum(),
+        Ty::Int(_) =>
+            unwrap_or_bug(builder.build_int_add(lhs.into_int_value(), rhs.into_int_value(), "int_add"), "int add")
+            .as_basic_value_enum(),
+        Ty::Float(_) =>
+            unwrap_or_bug(builder.build_float_add(lhs.into_float_value(), rhs.into_float_value(), "float_add"), "float add")
+            .as_basic_value_enum(),
         Ty::String =>
-            panic!("BUG: string concat not folded by ConstProp"),
-        _ => panic!("{:?}", _type)
+            bug!("string concat not folded by ConstProp"),
+        _ => bug!("unsupported type {:?} for sum operation", _type)
     }
 }
 
 fn build_sub<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => 
-            builder.build_int_sub(lhs.into_int_value(), rhs.into_int_value(), "int_sub")
-            .unwrap().as_basic_value_enum(),
-        Ty::Float(_) => 
-            builder.build_float_sub(lhs.into_float_value(), rhs.into_float_value(), "float_sub")
-            .unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) =>
+            unwrap_or_bug(builder.build_int_sub(lhs.into_int_value(), rhs.into_int_value(), "int_sub"), "int sub")
+            .as_basic_value_enum(),
+        Ty::Float(_) =>
+            unwrap_or_bug(builder.build_float_sub(lhs.into_float_value(), rhs.into_float_value(), "float_sub"), "float sub")
+            .as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for sub operation", _type)
     }
 }
 
 fn build_div<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => 
-            builder.build_int_signed_div(lhs.into_int_value(), rhs.into_int_value(), "int_div")
-            .unwrap().as_basic_value_enum(),
-        Ty::Float(_) => 
-            builder.build_float_div(lhs.into_float_value(), rhs.into_float_value(), "float_div")
-            .unwrap().as_basic_value_enum(),
-        _ => panic!("")
+        Ty::Int(_) =>
+            unwrap_or_bug(builder.build_int_signed_div(lhs.into_int_value(), rhs.into_int_value(), "int_div"), "int div")
+            .as_basic_value_enum(),
+        Ty::Float(_) =>
+            unwrap_or_bug(builder.build_float_div(lhs.into_float_value(), rhs.into_float_value(), "float_div"), "float div")
+            .as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for div operation", _type)
     }
 }
 
 fn build_mul<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => 
-            builder.build_int_mul(lhs.into_int_value(), rhs.into_int_value(), "int_mul")
-            .unwrap().as_basic_value_enum(),
-        Ty::Float(_) => 
-            builder.build_float_mul(lhs.into_float_value(), rhs.into_float_value(), "float_mul")
-            .unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) =>
+            unwrap_or_bug(builder.build_int_mul(lhs.into_int_value(), rhs.into_int_value(), "int_mul"), "int mul")
+            .as_basic_value_enum(),
+        Ty::Float(_) =>
+            unwrap_or_bug(builder.build_float_mul(lhs.into_float_value(), rhs.into_float_value(), "float_mul"), "float mul")
+            .as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for mul operation", _type)
     }
 }
 
 fn build_gt_compare<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => builder.build_int_compare(IntPredicate::SGT, lhs.into_int_value(), rhs.into_int_value(), "cmpres").unwrap().as_basic_value_enum(),
-        Ty::Float(_) => builder.build_float_compare(FloatPredicate::OGT, lhs.into_float_value(), rhs.into_float_value(), "cmpres").unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) => unwrap_or_bug(builder.build_int_compare(IntPredicate::SGT, lhs.into_int_value(), rhs.into_int_value(), "cmpres"), "int gt").as_basic_value_enum(),
+        Ty::Float(_) => unwrap_or_bug(builder.build_float_compare(FloatPredicate::OGT, lhs.into_float_value(), rhs.into_float_value(), "cmpres"), "float gt").as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for gt operation", _type)
     }
 }
 
 fn build_ge_compare<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => builder.build_int_compare(IntPredicate::SGE, lhs.into_int_value(), rhs.into_int_value(), "cmpres").unwrap().as_basic_value_enum(),
-        Ty::Float(_) => builder.build_float_compare(FloatPredicate::OGE, lhs.into_float_value(), rhs.into_float_value(), "cmpres").unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) => unwrap_or_bug(builder.build_int_compare(IntPredicate::SGE, lhs.into_int_value(), rhs.into_int_value(), "cmpres"), "int ge").as_basic_value_enum(),
+        Ty::Float(_) => unwrap_or_bug(builder.build_float_compare(FloatPredicate::OGE, lhs.into_float_value(), rhs.into_float_value(), "cmpres"), "float ge").as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for ge operation", _type)
     }
 }
 
 fn build_lt_compare<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => builder.build_int_compare(IntPredicate::SLT, lhs.into_int_value(), rhs.into_int_value(), "cmpres").unwrap().as_basic_value_enum(),
-        Ty::Float(_) => builder.build_float_compare(FloatPredicate::OLT, lhs.into_float_value(), rhs.into_float_value(), "cmpres").unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) => unwrap_or_bug(builder.build_int_compare(IntPredicate::SLT, lhs.into_int_value(), rhs.into_int_value(), "cmpres"), "int lt").as_basic_value_enum(),
+        Ty::Float(_) => unwrap_or_bug(builder.build_float_compare(FloatPredicate::OLT, lhs.into_float_value(), rhs.into_float_value(), "cmpres"), "float lt").as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for lt operation", _type)
     }
 }
 
 fn build_eq_compare<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) | Ty::Bool => builder.build_int_compare(IntPredicate::EQ, lhs.into_int_value(), rhs.into_int_value(), "cmpres").unwrap().as_basic_value_enum(),
-        Ty::Float(_) => builder.build_float_compare(FloatPredicate::UEQ, lhs.into_float_value(), rhs.into_float_value(), "cmpres").unwrap().as_basic_value_enum(),
-        _ => panic!("{:?}", _type)
+        Ty::Int(_) | Ty::Bool => unwrap_or_bug(builder.build_int_compare(IntPredicate::EQ, lhs.into_int_value(), rhs.into_int_value(), "cmpres"), "int eq").as_basic_value_enum(),
+        Ty::Float(_) => unwrap_or_bug(builder.build_float_compare(FloatPredicate::OEQ, lhs.into_float_value(), rhs.into_float_value(), "cmpres"), "float eq").as_basic_value_enum(),
+        _ => bug!("unsupported type {:?} for eq operation", _type)
     }
 }
 
 fn build_and<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Bool => builder.build_and(lhs.into_int_value(), rhs.into_int_value(), "ssl_and").unwrap().as_basic_value_enum(),
-        _ => panic!("Invalid type to process [and] operation.")
+        Ty::Bool => unwrap_or_bug(builder.build_and(lhs.into_int_value(), rhs.into_int_value(), "ssl_and"), "bool and").as_basic_value_enum(),
+        _ => bug!("invalid type {:?} to process and operation", _type)
     }
 }
 
 fn build_or<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Bool => builder.build_or(lhs.into_int_value(), rhs.into_int_value(), "ssl_or").unwrap().as_basic_value_enum(),
-        _ => panic!("Invalid type to process [or] operation.")
+        Ty::Bool => unwrap_or_bug(builder.build_or(lhs.into_int_value(), rhs.into_int_value(), "ssl_or"), "bool or").as_basic_value_enum(),
+        _ => bug!("invalid type {:?} to process or operation", _type)
     }
 }
 
 fn build_bit_xor<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
-        Ty::Int(_) => builder.build_xor(lhs.into_int_value(), rhs.into_int_value(), "xor").unwrap().as_basic_value_enum(),
-        _ => panic!("Invalid type to process [xor] operation.")
+        Ty::Int(_) => unwrap_or_bug(builder.build_xor(lhs.into_int_value(), rhs.into_int_value(), "xor"), "int xor").as_basic_value_enum(),
+        _ => bug!("invalid type {:?} to process xor operation", _type)
     }
 }
 
@@ -112,13 +114,13 @@ fn build_remainder<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueE
     match _type {
         Ty::Int(int_ty) => {
             if int_ty.is_signed() {
-                builder.build_int_signed_rem(lhs.into_int_value(), rhs.into_int_value(), "srem").unwrap().as_basic_value_enum()
+                unwrap_or_bug(builder.build_int_signed_rem(lhs.into_int_value(), rhs.into_int_value(), "srem"), "signed rem").as_basic_value_enum()
             } else {
-                builder.build_int_unsigned_rem(lhs.into_int_value(), rhs.into_int_value(), "urem").unwrap().as_basic_value_enum()
+                unwrap_or_bug(builder.build_int_unsigned_rem(lhs.into_int_value(), rhs.into_int_value(), "urem"), "unsigned rem").as_basic_value_enum()
             }
         },
-        Ty::Float(_) => builder.build_float_rem(lhs.into_float_value(), rhs.into_float_value(), "frem").unwrap().as_basic_value_enum(),
-        _ => panic!("Invalid type to process [rem] operation.")
+        Ty::Float(_) => unwrap_or_bug(builder.build_float_rem(lhs.into_float_value(), rhs.into_float_value(), "frem"), "float rem").as_basic_value_enum(),
+        _ => bug!("invalid type {:?} to process rem operation", _type)
     }
 }
 
@@ -126,40 +128,40 @@ fn build_bit_shift<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueE
     match _type {
         Ty::Int(ty) => {
             if left_shift {
-                builder.build_left_shift(lhs.into_int_value(), rhs.into_int_value(), "shl").unwrap().as_basic_value_enum()
+                unwrap_or_bug(builder.build_left_shift(lhs.into_int_value(), rhs.into_int_value(), "shl"), "left shift").as_basic_value_enum()
             } else {
-                builder.build_right_shift(lhs.into_int_value(), rhs.into_int_value(), ty.is_signed(), "shr").unwrap().as_basic_value_enum()
+                unwrap_or_bug(builder.build_right_shift(lhs.into_int_value(), rhs.into_int_value(), ty.is_signed(), "shr"), "right shift").as_basic_value_enum()
             }
         },
-        _ => panic!("Invalid type to process [bit shift] operation.")
+        _ => bug!("invalid type {:?} to process bit shift operation", _type)
     }
 }
 
 fn build_not_eq<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
         Ty::Int(_) | Ty::Bool => {
-            builder.build_int_compare(IntPredicate::NE, lhs.into_int_value(), rhs.into_int_value(), "ne")
-                .unwrap().as_basic_value_enum()
+            unwrap_or_bug(builder.build_int_compare(IntPredicate::NE, lhs.into_int_value(), rhs.into_int_value(), "ne"), "int ne")
+                .as_basic_value_enum()
         }
         Ty::Float(_) => {
-            builder.build_float_compare(FloatPredicate::UNE, lhs.into_float_value(), rhs.into_float_value(), "fne")
-                .unwrap().as_basic_value_enum()
+            unwrap_or_bug(builder.build_float_compare(FloatPredicate::ONE, lhs.into_float_value(), rhs.into_float_value(), "fne"), "float ne")
+                .as_basic_value_enum()
         }
-        _ => panic!("Invalid type to process [not equal] operation."),
+        _ => bug!("invalid type {:?} to process not equal operation", _type),
     }
 }
 
 fn build_le<'llvm>(builder: &Builder<'llvm>, _type: &Ty, lhs: BasicValueEnum<'llvm>, rhs: BasicValueEnum<'llvm>) -> BasicValueEnum<'llvm> {
     match _type {
         Ty::Int(_) | Ty::Bool => {
-            builder.build_int_compare(IntPredicate::SLE, lhs.into_int_value(), rhs.into_int_value(), "sle")
-                .unwrap().as_basic_value_enum()
+            unwrap_or_bug(builder.build_int_compare(IntPredicate::SLE, lhs.into_int_value(), rhs.into_int_value(), "sle"), "int le")
+                .as_basic_value_enum()
         }
         Ty::Float(_) => {
-            builder.build_float_compare(FloatPredicate::OLE, lhs.into_float_value(), rhs.into_float_value(), "ole")
-                .unwrap().as_basic_value_enum()
+            unwrap_or_bug(builder.build_float_compare(FloatPredicate::OLE, lhs.into_float_value(), rhs.into_float_value(), "ole"), "float le")
+                .as_basic_value_enum()
         }
-        _ => panic!("Invalid type to process [not equal] operation."),
+        _ => bug!("invalid type {:?} to process less equal operation", _type),
     }
 }
 
